63f1d233b67d8cd9e00f95640ea2498f0a672e13
Eloy Espinaco <eloyesp@gmail.com>
2015-01-07 19:58:23 -0300

    Add a map.

--- lib/server.rb
+++ lib/server.rb
@@ -1,3 +1,9 @@
+require 'cuba'
+
+Cuba.use Rack::Static, urls: [""],
+                       root: 'public',
+                       index: 'index.html'
+
 Cuba.define do
   on get do
     res.write "hello world"
