35288d077e1eda7e5e0204f67f52ce79583c8103
Eloy Espinaco <eloyesp@gmail.com>
2015-03-08 16:28:01 -0300

    Reload enabled!

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect');
 
+gulp.task('reload', function() {
+  gulp.src('public/**/*')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['public/**/*'], ['reload']);
+});
+
 gulp.task('server', function() {
   connect.server({
     root: 'public',
+    livereload: true,
     port: 8000
   });
-})
+});
 
-gulp.task('default', ['server']);
+gulp.task('default', ['server', 'watch']);
